56fb19dfa4a071075f10ca75c07af3227625cfe5
Tfauves <tfauvelle@gmail.com>
2021-07-12 21:59:29 -0400

    refactored engine instantiation in main.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,23 +10,19 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        Engine carEngine = new Engine("V6", false, 0);
-        Engine boatEngine  = new Engine("boat", false, 0);
-        Engine planeEngine = new Engine("jet", false, 0);
-        Engine electricEngine = new Engine("electric", false, 0);
 
         Tire carTire = new Tire(33, 35);
         Tire planeTire = new Tire(40, 200);
 
-        Car car01 = new Car(carEngine, "Audi A6", 4);
+        Car car01 = new Car(new Engine("V6", false, 0), "Audi A6", 4);
         car01.addTire(carTire, 4);
 
-        Car car02 = new Car(electricEngine, "tesla", 4);
+        Car car02 = new Car(new Engine("electric", false, 0), "tesla", 4);
         car02.addTire(carTire, 4);
 
-        Boat boat01 = new Boat(boatEngine, "pontoon", 12, 1000);
+        Boat boat01 = new Boat( new Engine("boat", false, 0), "pontoon", 12, 1000);
 
-        Plane plane01 = new Plane(planeEngine, "747", 524, false);
+        Plane plane01 = new Plane(new Engine("jet", false, 0), "747", 524, false);
         plane01.addTire(planeTire, 6);
 
         System.out.println(car01);
